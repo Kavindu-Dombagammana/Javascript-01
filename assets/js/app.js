eb110dadb65c75b32a03d5f140dce1cd40a9f185
Kavindu Prabashwara <godlikeboot@gmail.com>
2026-09-15 11:40:46 +0530

    comparison operators

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,30 +25,47 @@
 
 //Javascript is not type safe and can assign any to a variable(type safe means it can only assign only one data type like java)
 
-name = 10;
-console.log(name);
+// name = 10;
+// console.log(name);
 
-let cust_Name = "Saman";
-let cust_Age = 20;
-let customer = []; //This is an array
-let customers = {}; //This is an object
-let someVariable;
+// let cust_Name = "Saman";
+// let cust_Age = 20;
+// let customer = []; //This is an array
+// let customers = {}; //This is an object
+// let someVariable;
 
-console.log(typeof cust_Name);
-console.log(typeof cust_Age);
-console.log(typeof customer);
-console.log(typeof customers);
-console.log(typeof someVariable);
+// console.log(typeof cust_Name);
+// console.log(typeof cust_Age);
+// console.log(typeof customer);
+// console.log(typeof customers);
+// console.log(typeof someVariable);
 
 
-let num1;
-num1 = 10;
+// let num1;
+// num1 = 10;
 
-let num2;
-num2 = 20
-console.log(num1 + num2);
-console.log(num1 - num2);
-console.log(num1 * num2);
-console.log(num1 / num2);
-console.log(num1 % num2);
+// let num2;
+// num2 = 20
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
 
+// let x = 5;
+// let y = 3;
+// let text = "Hello";
+// let result = x + y;
+// console.log(result);
+
+// console.log(x+y+text);
+
+// comparisons
+// >     <     >=     <=     ==     ===     !=
+
+let x = 10;
+let y = "10";
+
+console.log(x == y); // output is true because it only checks the value 
+console.log(x === y);// output is false because it checks the data type too 
+//other operators works same as the java
