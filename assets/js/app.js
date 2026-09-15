c405eb0662bdfaa7208c3cfd852e2a8e6bc7527c
Kavindu Prabashwara <godlikeboot@gmail.com>
2026-09-15 10:46:16 +0530

    console logs and variables

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,45 @@
-console.log("Hello World!");
+//console.log("Hello World!");
 
-alert("This is an alert");
+//alert("This is an alert");
 //this is an alert and pops up when the site is refreshed
+
+//Can use both with or without a semicollen ";"
+//console.log("Hello");
+//console.log("World")
+//This is both true
+
+//Variables
+
+// let name = "Brutus";
+
+// console.log(name);
+// console.log(typeof name); // This prints the type of the variable name
+
+// let age = 22;
+// console.log(age);
+// console.log(typeof age); // This prints the type of the variable name
+
+// let male = true;
+// console.log(male);
+// console.log(typeof male); // This prints the type of the variable name
+
+//Javascript is not type safe and can assign any to a variable(type safe means it can only assign only one data type like java)
+
+name = 10;
+console.log(name);
+
+let cust_Name = "Saman";
+let cust_Age = 20;
+let customer = []; //This is an array
+let customers = {}; //This is an object
+let someVariable;
+
+console.log(typeof cust_Name);
+console.log(typeof cust_Age);
+console.log(typeof customer);
+console.log(typeof customers);
+console.log(typeof someVariable);
+
+
+
+
